1d45488468843fd38af51966e30555f191872f73
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-12 03:23:16 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+require 'pry'
+
+class School
+    
+    attr_accessor :school, :roster
+    
+    def initialize(name)
+      @school = name
+      @roster = {}
+    end
+  
+    def add_student(name, grade)
+      @roster[grade] ||= [] 
+      @roster[grade] << name
+    end
+    
+    def grade(grade)
+      @roster[grade]
+    end
+    
+    def sort
+        grades = @roster.collect do |grade, students| 
+          students.sort
+        end
+        binding.pry
+    end
+end
