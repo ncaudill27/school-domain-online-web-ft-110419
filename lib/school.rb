0df79a1dccb9c29f2d0794287c5fc94275d1e867
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-12 03:33:20 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -19,7 +19,7 @@ class School
     end
     
     def sort
-        @roster. do |grade, students|
+        @roster.sort do |grade, students|
           binding.pry
           students.sort
         end
